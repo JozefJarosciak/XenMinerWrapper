50f7ba50d216d6008540a9dea5f9d62d9e312759
jozef_jarosciak <jarosciak@gmail.com>
2023-09-15 21:13:51 -0400

    + Multithreaded Found Block counter

--- XenMinerWrapper.py
+++ XenMinerWrapper.py
@@ -1,4 +1,5 @@
 import threading
+import time
 import tkinter as tk
 from tkinter import ttk, messagebox
 import requests
@@ -13,6 +14,8 @@ class MinerApp(tk.Tk):
 
     def __init__(self):
         super().__init__()
+        self.valid_hash_count = 0
+        self.valid_hash_count_var = tk.StringVar()
         self.lock = threading.Lock()
 
         self.title("XEN.pub's XenMiner Wrapper")
@@ -40,6 +43,7 @@ class MinerApp(tk.Tk):
         self.python_env = tk.Entry(self, width=70)
         self.python_env.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
+
         # Load saved python environment path
         self.load_python_env()
         # Load saved ethereum address
@@ -51,6 +55,10 @@ class MinerApp(tk.Tk):
         self.num_parallel = ttk.Combobox(self, values=[str(i) for i in range(1, max_parallel+1)])
         self.num_parallel.set("1")
         self.num_parallel.grid(row=3, column=1, padx=10, pady=10, sticky="w")
+        self.parse_output_var = tk.BooleanVar(value=False)
+        self.parse_output_chk = tk.Checkbutton(self, text="Parse Hash/s", variable=self.parse_output_var)
+        self.parse_output_chk.grid(row=3, column=1, padx=5, pady=2)
+
 
         # Frame for the buttons
         self.button_frame = tk.Frame(self)
@@ -58,22 +66,33 @@ class MinerApp(tk.Tk):
 
         # Run script button inside the frame
         self.run_btn = tk.Button(self.button_frame, text="Run", command=self.run_script)
-        self.run_btn.grid(row=0, column=0, padx=5)
+        self.run_btn.grid(row=4, column=0, padx=5)
 
         # Stop script button inside the frame
         self.stop_btn = tk.Button(self.button_frame, text="Stop", command=self.stop_script, state=tk.DISABLED)
-        self.stop_btn.grid(row=0, column=1, padx=5)
+        self.stop_btn.grid(row=4, column=1, padx=5)
+
+        # Separate Frame for Counters
+        self.counter_frame = tk.Frame(self)
+        self.counter_frame.grid(row=5, column=0, columnspan=2, pady=1)
+        self.valid_hashes_label = tk.Label(self.counter_frame, text="0", textvariable=self.valid_hash_count_var, font=("Arial", 12, "bold"))
+        self.valid_hashes_label.grid(row=0, column=1, padx=2, pady=2, sticky="w")
+        self.valid_hash_count_var.set(f"Found Blocks: {self.valid_hash_count}")
+
+
 
         # Tabs for parallel executions
         self.tab_control = ttk.Notebook(self)
-        self.tab_control.grid(row=5, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
-        self.grid_rowconfigure(5, weight=1)  # Make sure the notebook expands vertically
+        self.tab_control.grid(row=6, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+        self.grid_rowconfigure(6, weight=1)  # Make sure the notebook expands vertically
         self.grid_columnconfigure(1, weight=1)  # Make sure the notebook expands horizontally
 
+        self.footer_frame.grid(row=7, column=0, columnspan=2)
 
 
         # List to store running processes
         self.running_processes = []
+
 
 
 
@@ -154,8 +173,30 @@ class MinerApp(tk.Tk):
 
     def run_miner_script(self, output_widget):
         python_env = self.python_env.get().strip()
+        def get_hash_per_second(line):
+            try:
+                # Split by ', ' then pick the second part (i.e., the hash/s part)
+                hash_part = line.split(', ')[1]
+
+                # Now, split the hash_part by "hash/s" to get the number
+                number_str = hash_part.split('hash/s')[0].strip()
+
+                # Convert to float and format
+                number = float(number_str)
+                formatted_number = '{:,.2f}'.format(number)
+
+                return formatted_number
+            except:
+                return None
+
+        def extract_difficulty(line):
+            if "Updating difficulty" in line:
+                return line.split("Updating difficulty")[1].strip()
+            return None
 
         def run():
+            last_hash_per_second = None
+
             process = subprocess.Popen(
                 [python_env, 'miner.py'],
                 stdout=subprocess.PIPE,
@@ -172,11 +213,32 @@ class MinerApp(tk.Tk):
                 line = process.stdout.readline()
                 if not line:
                     break
+                # Append the line to the Text widget if Parse Output is checked
+                if "valid hash" in line:
+                    print("Detected a valid hash (mined a new block)!")
+                    with self.lock:
+                        self.valid_hash_count += 1
+                    output_widget.after(0, self.update_ui_counters)
+                    continue
+                if self.parse_output_var.get():
+                    hash_per_second = get_hash_per_second(line)
+                    # Only update if hash rate is different from the last one
+                    if hash_per_second and hash_per_second != last_hash_per_second:
+                        output_widget.insert(tk.END, hash_per_second + " hash/s\n")
+                        output_widget.yview(tk.END)  # Auto-scroll to the bottom
+                        last_hash_per_second = hash_per_second
+                    output_widget.yview(tk.END)  # Auto-scroll to the bottom
+                    #time.sleep(0.2)
+                else:
+                    output_widget.insert(tk.END, line)
+                    output_widget.yview(tk.END)  # Auto-scroll to the bottom
 
-                # Append the line to the Text widget
-                # We'll use the widget's own method to ensure that GUI updates are thread-safe
-                output_widget.insert(tk.END, line)
-                output_widget.yview(tk.END)  # Auto-scroll to the bottom
+                difficulty_update = extract_difficulty(line)
+                if difficulty_update:
+                    output_widget.insert(tk.END, "Updating difficulty " + difficulty_update + "\n")
+                    output_widget.yview(tk.END)  # Auto-scroll to the bottom
+                    continue
+                self.after(0, self.update_ui_counters)
 
             process.wait()  # Wait until the process completes
 
@@ -185,9 +247,8 @@ class MinerApp(tk.Tk):
         threading.Thread(target=run, daemon=True).start()
 
 
-        # Using a thread to avoid blocking the main UI
-        threading.Thread(target=run, daemon=True).start()
-
+    def update_ui_counters(self):
+        self.valid_hash_count_var.set(f"Found Blocks: {self.valid_hash_count}")
 
 
     def open_webpage(self, url):
@@ -243,8 +304,6 @@ class MinerApp(tk.Tk):
 
         # Make sure this tab is shown in front of all others
         self.tab_control.select(tab)
-
-
 
 
 
